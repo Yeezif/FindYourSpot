596c6b1af312bf89cb9c9fe0d35d556a0574c58e
Yeezif <yunus.a.ata@gmail.com>
2025-05-19 20:13:26 +0200

    animated center button

--- frontend/lib/widgets/buttons/map/center_location_button.dart
+++ frontend/lib/widgets/buttons/map/center_location_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class CenterLocationButton extends StatelessWidget {
+class CenterLocationButton extends StatefulWidget {
 
   final LatLng? currentLocation;
   final MapController mapController;
@@ -16,6 +16,85 @@ class CenterLocationButton extends StatelessWidget {
   });
 
   @override
+  _CenterLocationButtonState createState() => _CenterLocationButtonState();
+
+}
+
+class _CenterLocationButtonState extends State<CenterLocationButton> with TickerProviderStateMixin{
+
+  late AnimationController _controller;
+  late Animation<LatLng> _LatLngAnimation;
+  late Animation<double> _zoomAnimation;
+  late LatLng _currentCenter;
+  late double _currentZoom;
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget.mapController.mapEventStream.listen((event) {
+      if (event is MapEventMove || event is MapEventMoveEnd) {
+        setState(() {
+          _currentCenter = event.camera.center;
+          _currentZoom = event.camera.zoom;
+        });
+      }
+    });
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),  
+    );
+
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_handleAnimation);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _flyTo(LatLng targetLocation, double targetZoom) {
+
+    _controller.reset();
+
+    final cameraCenter = _currentCenter;
+    final cameraZoom = _currentZoom;
+
+    _LatLngAnimation = LatLngTween(
+      begin: cameraCenter,
+      end: targetLocation,
+    ).animate(CurvedAnimation(
+      parent: _controller, 
+      curve: Curves.easeOut)
+    );
+
+    _zoomAnimation = Tween<double>(
+      begin: cameraZoom,
+      end: targetZoom,
+    ).animate(CurvedAnimation(
+      parent: _controller, 
+      curve: Curves.easeOut)
+    );
+
+    // Remove previous listeners, if any
+    _controller.removeListener(_handleAnimation);
+
+    // Add listener now
+    _controller.addListener(_handleAnimation);
+
+    _controller.forward(from: 0.0);
+  }
+
+  void _handleAnimation() {
+    final latLng = _LatLngAnimation.value;
+    final zoom = _zoomAnimation.value;
+    widget.mapController.move(latLng, zoom);
+  }
+
+
+  @override
   Widget build(BuildContext context) {
 
     return FloatingActionButton(
@@ -24,8 +103,10 @@ class CenterLocationButton extends StatelessWidget {
       backgroundColor: Colors.white,
       elevation: 4.0,
       onPressed: () {
-        if (currentLocation != null) {
-          mapController.move(currentLocation!, 15);
+        if (widget.currentLocation != null) {
+          if (widget.currentLocation != widget.mapController.camera.center) {
+            _flyTo(widget.currentLocation!, 15.0);
+          }
         }
       },
       child: const Icon(
@@ -34,6 +115,23 @@ class CenterLocationButton extends StatelessWidget {
       ) // == Colors.blueGrey[400]
     );
 
+  }
+
+}
+
+class LatLngTween extends Tween<LatLng> {
+
+  LatLngTween({
+    required LatLng begin,
+    required LatLng end
+  }) : super(begin: begin, end: end);
+
+  @override
+  LatLng lerp(double t) {
+    return LatLng(
+      begin!.latitude + (end!.latitude - begin!.latitude) * t, 
+      begin!.longitude + (end!.longitude - begin!.longitude) * t
+    );
   }
 
 }
