5902c03ee1ee48fa085aba2f0ee71b12aeec5fa0
Yeezif <yunus.a.ata@gmail.com>
2025-05-26 18:47:49 +0200

    deleted unessecary line

--- frontend/lib/widgets/buttons/map/create_spot_button.dart
+++ frontend/lib/widgets/buttons/map/create_spot_button.dart
@@ -1,7 +1,6 @@
 import 'package:findyourspot/widgets/widgets/create_spot_form.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:flutter_map/flutter_map.dart';
 
 class CreateSpotButton extends StatelessWidget {
 
